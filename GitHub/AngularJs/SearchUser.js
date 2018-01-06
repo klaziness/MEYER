46c1bd3f540d718d94bc88c0758383eb2583c8c1
klaziness <klaziness@gmail.com>
2018-01-06 10:22:06 +0700

    Y
    Y

--- GitHub/AngularJs/SearchUser.js
+++ GitHub/AngularJs/SearchUser.js
@@ -22,7 +22,6 @@ SearchUser.controller('mainController', function ($scope, $http) {
         $http.get(url + keySearch)
 
         .success(function (result) {
-            console.log(result.items);
             // loop
             angular.forEach(result.items, function (item) {
                 var valObj = {};
@@ -32,13 +31,11 @@ SearchUser.controller('mainController', function ($scope, $http) {
                 valObj.Pic = item.avatar_url;
                 valObj.R_epo = item.repos_url;
 
-                console.log(item.repos_url);
-
                 $scope.listUser.push(valObj);
             });
             // loop
 
-            console.log($scope.listUser);
+            console.log('item-array => ' + $scope.listUser);
 
         })
         .error(function (data) {
@@ -57,7 +54,40 @@ SearchUser.controller('mainController', function ($scope, $http) {
 
     }
 
-    $scope.clickViewRepo = function (url) {
-        console.log(url);
+    $scope.clickViewRepo = function (url, name) {
+
+        $scope.userName = name;
+        $scope.v_Repo = [];
+
+        //console.log('url => ' + url);
+
+        // ex para url : https://api.github.com/users/msintuneappsdk/repos
+        $http.get(url)
+        .success(function (resultFull) {
+
+            angular.forEach(resultFull, function (item) {
+
+                var v_valObj = {};
+
+                v_valObj.v_name = item.name;
+                v_valObj.v_description = item.description;
+                v_valObj.v_stargazers_count = item.stargazers_count;
+                v_valObj.v_forks_count = item.forks_count;
+
+                if (item.language === null) {
+                    v_valObj.v_checkUndefined = false;
+                } else {
+                    v_valObj.v_checkUndefined = true;
+                };
+
+                v_valObj.v_language = item.language;
+
+                // add obj to array
+                $scope.v_Repo.push(v_valObj);
+            });
+        })
+        .error(function (data) {
+            console.log(data);
+        });
     };
 });
